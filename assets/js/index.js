b51715574ddc3795e0484c6884ddb51af0d0ba84
NguyenThanhPhat <thanhphatnguyen82gmail.com>
2022-11-08 12:41:49 +0700

    upload heart

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@
     */
     var settings = {
         particles: {
-            length:   200, // maximum amount of particles
+            length:   500, // maximum amount of particles
             duration:   2, // particle duration in sec
             velocity: 100, // particle velocity in pixels/sec
             effect: -0.75, // play with this for a nice effect
